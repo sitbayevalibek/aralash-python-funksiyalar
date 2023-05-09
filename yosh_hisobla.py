7c983341b99c783d286924c86c868507d253afc3
sitbayevalibek <dizdada1881@gmail.com>
2023-05-09 14:39:43 +0500

    yosh_hisobla
    Yosh hisoblaydigan funksiya

---
+++ yosh_hisobla.py
@@ -0,0 +1,4 @@
+def yosh_hisobla(tugilgan_yil, joriy_yil=2023):
+    """Foydalanuvchi tug'ilgan yilidan uning yoshini hisoblaydi"""
+    print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+yosh_hisobla(1957)
