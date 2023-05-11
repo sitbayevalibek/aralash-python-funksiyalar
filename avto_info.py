0ca113acba11db5755b5cb05ce210d46d2ef87bd
sitbayevalibek <dizdada1881@gmail.com>
2023-05-11 14:53:47 +0500

    avto_info

---
+++ avto_info.py
@@ -0,0 +1,40 @@
+def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+    avto = {'kompaniya':kompaniya,
+            'model':model,
+            'rang':rangi,
+            'korobka':korobka,
+            'yil':yili,
+            'narh':narhi}
+    return avto
+
+print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
+avtolar=[] # salondagi avtolar uchun bo'sh ro'yxat
+while True:
+    print("\nQuyidagi ma'lumotlarni kiriting\n",end='')
+    kompaniya=input("Ishlab chiqaruvchi: ")
+    model=input("Modeli: ")
+    rangi=input("Rangi: ")
+    korobka=input("Korobka: ")
+    yili=input("Ishlab chiqarilgan yili: ")
+    narhi=input("Narhi: ")
+    
+    #Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida 
+    #lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
+    avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
+    
+    # Yana avto qo'shish-qo'shmaslikni so'raymiz
+    javob = input("Yana avto qo'shasizmi? (yes/no): ")
+    if javob=='no':
+        break
+
+# Ro'yxatdagi avtolar haqida ma'lumot chiqaramiz
+print("\nSalondagi avtolar:")
+for avto in avtolar:
+    narh = avto.get('narh')
+    if narh!=None:
+        narh = f"{narh} $"
+    else:
+        narh = "Noma'lum"
+    print(f"{avto['rang'].title()} {avto['kompaniya'].upper()} "
+          f"{avto['model'].capitalize()}, {avto['korobka']} korobka, "
+          f"{avto['yil']}-yil, {narh}")    
